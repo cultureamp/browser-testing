6d27aef36e1901fd8b422788c5f04e6280c9b5b8
kunwardeep singh bedi <kunwardeep.bedi@cultureamp.com>
2018-09-04 17:47:45 +1000

    Added ability to run tests locally

---
+++ config/wdio/wdio.local.conf.js
@@ -0,0 +1,51 @@
+// const Promise = require('es6-promise').Promise;
+const selenium = require('selenium-standalone');
+const startSeleniumServer = () => {
+  return new Promise((resolve, reject) => {
+    selenium.start((err, process) => (err ? reject(err) : resolve(process)));
+  });
+};
+
+let seleniumProcess;
+
+exports.config = {
+  baseUrl: 'http://www.cultureamp.design',
+  framework: 'mocha',
+  waitforTimeout: 60000,
+  logLevel: 'error',
+  coloredLogs: true,
+  reporters: ['dot', 'spec'],
+  specs: ['./tests/*.test.js'],
+  maxInstances: 20,
+  capabilities: [
+    {
+      browserName: 'chrome',
+      chromeOptions: {
+        args: ['no-sandbox', 'disable-web-security', 'headless']
+      }
+    },
+    {
+      browserName: 'firefox',
+      'moz:firefoxOptions': {
+        args: ['-headless']
+      }
+    }
+  ],
+  mochaOpts: {
+    timeout: 40000,
+    compilers: ['js:babel-core/register']
+  },
+  onPrepare: () => {
+    return startSeleniumServer().then(process => {
+      seleniumProcess = process;
+    });
+  },
+  onComplete: () => {
+    console.log('Shutting down Selenium');
+    seleniumProcess.kill();
+  },
+  before: () => {
+    const chai = require('chai');
+    global.expect = chai.expect;
+  }
+};
