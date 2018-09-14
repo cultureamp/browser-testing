ecd682ba822d3aaf4459d4c24ae6f8b399833875
kunwardeep singh bedi <kunwardeep.bedi@cultureamp.com>
2018-09-14 13:38:16 +1000

    Fix broken test

--- tests/functional/example1.test.js
+++ tests/functional/example1.test.js
@@ -1,7 +1,8 @@
 describe('CultureAmp Design website style page 1', function() {
   it('shows the text Visuals', function() {
-    browser.url('/styles');
-    const text = browser.getText('[id=visuals]');
-    expect(text).to.equal('Visuals');
+    return browser
+      .url('/styles')
+      .getText('[id=visuals]')
+      .then(text => expect(text).to.equal('Visuals'));
   });
 });
