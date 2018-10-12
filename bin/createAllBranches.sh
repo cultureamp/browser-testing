81f4370c2b8ad7ff94b6b5a88c4cdb05b5746715
kunwardeep singh bedi <kunwardeep.bedi@cultureamp.com>
2018-10-12 14:46:01 +1100

    Skip running visual tests

--- bin/createAllBranches.sh
+++ bin/createAllBranches.sh
@@ -2,6 +2,10 @@
 
 # Script is from - https://stackoverflow.com/questions/34405047/how-do-you-merge-into-another-branch-using-travis-with-git-commands
 
+# ==========
+# We need to run this script as we need to find out parent branch (COMPARE_SHA) and branch name. This can only be done if we have origin/master locally
+# ==========
+
 # Keep track of where Travis put us.
 # We are on a detached head, and we need to be able to go back to it.
 local build_head=$(git rev-parse HEAD)
