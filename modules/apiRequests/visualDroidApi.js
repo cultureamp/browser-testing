96c81d633fcc4b6c7e733b2471376f29115f62f8
kunwardeep singh bedi <kunwardeep.bedi@cultureamp.com>
2018-10-12 14:42:41 +1100

    Removed buildkite variables

--- modules/apiRequests/visualDroidApi.js
+++ modules/apiRequests/visualDroidApi.js
@@ -5,12 +5,12 @@ const VISUAL_DROID_PASSWORD = process.env.VISUAL_DROID_PASSWORD;
 const PROJECT_NAME = process.env.PROJECT_NAME;
 const BRANCH_SHA = process.env.BRANCH_SHA;
 const COMPARE_SHA = process.env.COMPARE_SHA;
-const BUILDKITE = process.env.BUILDKITE;
+const CI = process.env.CI;
 const SAVE_IMAGE_URL = `${URL_VISUAL_DROID}/saveImage`;
 const SAVE_AND_COMPARE_IMAGE_URL = `${URL_VISUAL_DROID}/saveAndCompareImage`;
 
 const request = axios.create({
-  baseURL: BUILDKITE ? SAVE_IMAGE_URL : SAVE_AND_COMPARE_IMAGE_URL,
+  baseURL: CI ? SAVE_IMAGE_URL : SAVE_AND_COMPARE_IMAGE_URL,
   auth: {
     password: VISUAL_DROID_PASSWORD,
   },
@@ -39,7 +39,7 @@ exports.saveImage = (imgBase64str, imageKey, metadata, compareOptions) => {
     if (typeof data.errorMessage !== 'undefined') {
       throw new Error(`Visual comparison fail - ${data.errorMessage}`);
     }
-    if (!BUILDKITE) {
+    if (!CI) {
       // eslint-disable-next-line no-console
       console.info(data);
       return data.upload && data.imagesMatch;
